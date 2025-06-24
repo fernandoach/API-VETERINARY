fe7d48e786b1237c7c634de2a4b7c4fb781d222e
Fernando A. <fernandoach333@gmail.com>
2025-06-23 23:01:01 -0500

    ADD: authorization middlewares (user, veterinary, admin)

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,10 @@
+import { addMinutes } from 'date-fns'
 import { registerUser } from '../repositories/userRepository/registerUser.js'
 import { hashPassword } from '../utils/hashPassword.js'
+import { appointmentSchema } from '../validations/appointmentSchema.js'
 import { userSchema } from '../validations/userSchema.js'
+import { verifySchedule } from '../repositories/appointmentRepository/verifySchedule.js'
+import { registerAppointment } from '../repositories/appointmentRepository/registerAppointment.js'
 
 async function userRegisterController (req, res) {
   try {
@@ -20,4 +24,29 @@ async function userRegisterController (req, res) {
   }
 }
 
-export { userRegisterController }
+async function userCreateAppointmentController (req, res) {
+  try {
+    const { date, startTime, reason, idVeterinary, idPet } = req.body
+
+    await appointmentSchema.validateAsync({ date, startTime, reason, state: 'P', idVeterinary, idPet })
+
+    const [year, month, day] = date.split('-')
+    const [hour, minutes] = startTime.split(':')
+    const endTime = addMinutes(new Date(year, month, day, hour, minutes, 0), 60)
+    const stateSchedule = await verifySchedule(date, idVeterinary, endTime, startTime)
+    console.log(stateSchedule)
+    if (stateSchedule) {
+      await registerAppointment(date, startTime, endTime, reason, idVeterinary, idPet)
+      return res.send({ message: `Cita registrada para: ${year}-${month}-${day} a las ${hour}:${minutes} hasta las ${endTime.getHours().toString().padStart(2, '0')}:${endTime.getMinutes().toString().padStart(2, '0')}` })
+    } else {
+      return res.status(400).send({ message: 'Horario con el veterinario seleccionado no disponible.' })
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ message: error.message })
+    }
+    return res.status(400).json(error.details[0].message ? { message: error.details[0].message } : { message: 'Error inesperado' })
+  }
+}
+
+export { userRegisterController, userCreateAppointmentController }
--- src/repositories/userRepository/registerUser.js
+++ src/repositories/userRepository/registerUser.js
@@ -9,16 +9,22 @@ async function registerUser (firstname, lastname, gender, birthday, dni, telepho
         UUID(), ?, ?, ?, ?, ?, ?, ?, ?
       );
     `
-
     const connection = await createConnection()
     const [result] = await connection.query(sql, [
       firstname, lastname, gender, birthday, dni, telephone, email, password
     ])
-    console.log(result)
-
     return result
   } catch (error) {
-    return error
+    if (error.code === 'ER_DUP_ENTRY') {
+      throw new Error('El correo o DNI proporcionado ya está registrado.')
+    }
+    if (error.code === 'ER_NO_SUCH_TABLE') {
+      throw new Error('La tabla no existe en la base de datos.')
+    }
+    if (error.code.includes('ER_NO_REFERENCED_ROW')) {
+      throw new Error('Mascota o veterinario no existentes.')
+    }
+    throw new Error(error)
   }
 }
 
